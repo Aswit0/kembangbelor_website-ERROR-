712588342fc4f27c5ad54424b345b84736090cd2
Aswit0 <dimasaswito@gmail.com>
2024-02-24 14:04:54 +0700

    add navbar and landing

---
+++ resources/views/home/home.blade.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="build/assets/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+
+  <nav class="navbar navbar-expand-lg bg-transparent sticky-top">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">
+        <img src="build/assets/logo.png" alt="Logo" width="50" height="50" class="d-inline-block align-text-top">
+      </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse justify-content-center" id="navbarNavDropdown">
+        <ul class="navbar-nav">
+          <li class="nav-item"><a class="nav-link" href="#">Beranda</a></li>
+          <li class="nav-item"><a class="nav-link" href="#">Wisata</a></li>
+          <li class="nav-item"><a class="nav-link" href="#">Kritik dan Saran</a></li>
+          <li class="nav-item"><a class="nav-link" href="#">360</a></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+
+  <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+            <img class="d-block w-100" src="build/assets/home1.jpg" alt="First slide">
+            <div class="carousel-caption d-md-block">
+                <h2>Ayo Liburan Di</h2>
+                <h1>Desa<br>Kembang Belor</h1>
+                <p>Sebuah desa di kecamatan Pacet yang memiliki keindahan yang luar biasa dari berbagai bidang. Mulai dari fasilitas yang lengkap, penginapan dan banyak wisata yang sangat indah untuk dinikmati</p>
+                <button class="btn btn-success">Ayo Jelajahi</button>
+            </div>
+        </div>
+        <div class="carousel-item">
+            <img class="d-block w-100" src="build/assets/home2.jpg" alt="Second slide">
+            <div class="carousel-caption d-md-flex justify-content-center align-items-center">
+              <div class="text-center">
+                  <h2>Ayo Liburan Di</h2>
+                  <h1>Desa<br>Kembang Belor</h1>
+                  <p>Sebuah desa di kecamatan Pacet yang memiliki keindahan yang luar biasa dari berbagai bidang. Mulai dari fasilitas yang lengkap, penginapan dan banyak wisata yang sangat indah untuk dinikmati</p>
+                  <button class="btn btn-success">Ayo Jelajahi</button>
+              </div>
+          </div>
+        </div>
+        <div class="carousel-item">
+            <img class="d-block w-100" src="build/assets/home3.jpg" alt="Third slide">
+            <div class="carousel-caption d-md-block">
+                <h2>Ayo Liburan Di</h2>
+                <h1>Desa<br>Kembang Belor</h1>
+                <p>Sebuah desa di kecamatan Pacet yang memiliki keindahan yang luar biasa dari berbagai bidang. Mulai dari fasilitas yang lengkap, penginapan dan banyak wisata yang sangat indah untuk dinikmati</p>
+                <button class="btn btn-success">Ayo Jelajahi</button>
+            </div>
+        </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev"></button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next"></button>
+</div>
+
+
+
+<section id="services" class="services section-bg">
+  <div class="container" data-aos="fade-up">
+
+    <div class="section-header">
+      <h2>Services</h2>
+      <p>Voluptatem quibusdam ut ullam perferendis repellat non ut consequuntur est eveniet deleniti fignissimos eos
+        quam</p>
+    </div>
+
+    <div class="row gy-4">
+
+      <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="100">
+        <div class="service-item  position-relative">
+          <div class="icon">
+            <i class="fa-solid fa-mountain-city"></i>
+          </div>
+          <h3>Nesciunt Mete</h3>
+          <p>Provident nihil minus qui consequatur non omnis maiores. Eos accusantium minus dolores iure perferendis
+            tempore et consequatur.</p>
+          <a href="service-details.html" class="readmore stretched-link">Learn more <i
+              class="bi bi-arrow-right"></i></a>
+        </div>
+      </div><!-- End Service Item -->
+
+      <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="200">
+        <div class="service-item position-relative">
+          <div class="icon">
+            <i class="fa-solid fa-arrow-up-from-ground-water"></i>
+          </div>
+          <h3>Eosle Commodi</h3>
+          <p>Ut autem aut autem non a. Sint sint sit facilis nam iusto sint. Libero corrupti neque eum hic non ut
+            nesciunt dolorem.</p>
+          <a href="service-details.html" class="readmore stretched-link">Learn more <i
+              class="bi bi-arrow-right"></i></a>
+        </div>
+      </div><!-- End Service Item -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
+</html>
